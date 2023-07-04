0fad657c9fa8a8ffe74b9f18366506aa75abb569
DmitryIGRD <133213651+DmitryIGRD@users.noreply.github.com>
2023-07-04 23:16:18 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -6,27 +6,4 @@ app.conf.result_backend = 'rpc://'
 
 @app.task(name='task_1.calculate_pi')
 def calculate_pi():
-    result = math.pi
-    from task_1 import process_result
-    process_result(result)
-    return result
-
-@app.task(name='task_1.calculate_factorial')
-def calculate_factorial(number):
-    factorial = math.factorial(number)
-    print(f"Факториал числа {number} равен {factorial}")
-    return factorial
-
-@app.task(name='task_1.calculate_square')
-def calculate_square(number):
-    square = number ** 2
-    print(f"Квадрат числа {number} равен {square}")
-    return square
-
-@app.task(name='task_1.periodic_task')
-def periodic_task():
-    print("Это периодическая задача, которая выполняется каждые 60 секунд.")
-
-@app.task(name='task_1.process_result')
-def process_result(result):
-    print("Результат задачи:", result)
+    return math.pi
